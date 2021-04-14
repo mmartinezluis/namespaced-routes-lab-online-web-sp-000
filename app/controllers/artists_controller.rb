1c298ba46c475e9ea7ca3476db4f3ebd93ee1eb7
Luis Martinez <luis_mmartinez@live.com>
2021-04-13 20:34:05 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.first.allow_create_artists
+      @artist = Artist.new
+    else
+      flash[:alert] = "Creation of new artists is not currently allowed at this time."
+      redirect_to artists_path
+    end
   end
 
   def create
